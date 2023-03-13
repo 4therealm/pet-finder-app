6c222c367f6a7a5feeb5b8ef1ca6f0b86c203025
EshuShango <tobiakinyemi21@gmail.com>
2023-03-13 15:06:12 -0500

    messing with header- testing

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import { Header, Wrapper, Feed,} from "./components";
+import { Header, Wrapper, } from "./components";
 import { HomePage, LoginPage, ProfilePage, SignupPage } from "./pages";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles/global.css";
 import {AppProvider} from "./utils/AppContext"
 import AddPetForm from "./pages/forms/AddPetForm";
+// import newMenu from "./components/exm -Header";
 
 
 
@@ -19,11 +20,14 @@ function App() {
         <Header />
           <div className="pt-3 px-4">
             <Routes>
+            {/* <newMenu> */}
+
               <Route path="/" element={<HomePage />} />
               <Route path="/login" element={<LoginPage />} />
               <Route path="/profile/:id" element={<ProfilePage />} />
               <Route path="/signup" element={<SignupPage />} />
               <Route path="/addPetForm" element={<AddPetForm />} />
+            {/* </newMenu> */}
             </Routes>
           </div>
       </Wrapper>
