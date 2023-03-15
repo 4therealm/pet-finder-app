9d89c61c42dae0eb550cf8a6ea70294652358059
Kenny Cordero <geckob4i@gmail.com>
2023-03-15 15:57:45 -0500

    added nice black background, fixed navigation sizing, added Max's prefered coloring to the page (blue, orange, purple), added box shadows to card components, added margins to cards as well

--- client/src/components/PetList.jsx
+++ client/src/components/PetList.jsx
@@ -9,6 +9,14 @@ const PetAside=({pets}) => {
   const [modalVisible,setModalVisible]=useState(false)
   const [petInfo,setPetInfo]=useState({})
   const {user, userLocation}=useAppCtx()
+  const cardStyle = {
+    boxShadow: '5px 5px 10px orange, -5px -5px 10px blue, 0px 0px 10px purple',
+    borderRadius: '5px',
+    padding: '10px',
+    display: 'inline-block',
+    margin: '10px',
+    textAlign: 'center',
+  };
 
 
   const handlePetButtonClick = async (petId) => {
@@ -170,7 +178,7 @@ const PetAside=({pets}) => {
       <h2>Your Pet List</h2>
       <div className="btn-group-vertical">
         {pets.map((pet) => (
-            <React.Fragment key={pet._id}>
+            <div style={cardStyle} key={pet._id}>
               <img src={pet.petImageUrl} alt="pet" />
               <button
                 key={pet._id}
@@ -180,7 +188,7 @@ const PetAside=({pets}) => {
               >
                 {pet.name}
               </button>
-            </React.Fragment>
+            </div>
           ))}
 
    
